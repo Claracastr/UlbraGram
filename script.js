ba33ece44fd999838364f2fa646f16748c292dfe
Ulbra <clarinha.palmas@gmail.com>
2023-07-14 16:25:18 -0300

    feat(home):criação da animação do botao de curtir

---
+++ script.js
@@ -0,0 +1,47 @@
+var likeIcons=document.querySelectorAll('.bi.bi-heart');
+
+likeIcons.forEach(function(likeIcon){
+    var postId=likeIcon.id;
+    var liked=localStorage.getItem(postId);
+
+    if(liked === 'true') {
+        likeIcon.classList.add('bi-heart-fill');
+        likeIcon.classList.remove('bi-heart');
+    }
+    
+    likeIcon.addEventListener('click',function() {
+        this.classList.toggle('bi-heart');
+        this.classList.toggle('bi-heart-fill');
+
+        if(this.classList.contains('bi-heart-fill')){
+            localStorage.setItem(postId,'true');
+        } else {
+            localStorage.setItem(postId,'false');
+        }
+
+    });
+});
+
+var bookmarkIcons=document.querySelectorAll('.bi.bi-bookmark')
+
+bookmarkIcons.forEach(function(bookmarkIcon){
+    var postId=bookmarkIcon.id;
+    var bookmark=localStorage.getItem(postId);
+
+    if(bookmark === 'true') {
+        bookmarkIcon.classList.add('bi-bookmark-fill');
+        bookmarkIcon.classList.remove('bi-bookmark');
+    }
+    
+    bookmarkIcon.addEventListener('click',function() {
+        this.classList.toggle('bi-bookmark');
+        this.classList.toggle('bi-bookmark-fill');
+
+        if(this.classList.contains('bi-bookmark-fill')){
+            localStorage.setItem(postId,'true');
+        } else {
+            localStorage.setItem(postId,'false');
+        }
+
+    });
+});
